f99bb0079e461f53f2e21e81a35462181afb975f
Zeke <egonzalezhidalgo@student.fullsail.edu>
2024-08-30 14:17:58 +0000

    Folders fixed and initial Twig files connected

--- Assignments/Star-Tracker/index.js
+++ Assignments/Star-Tracker/index.js
@@ -1,38 +1,47 @@
 // Load in our Express framework
-const express = require(`express`)
+const express = require(`express`);
+
+//load DotEnv
+const dotenv = require("dotenv");
+dotenv.config();
+
+//Set Port wit DotEnv
+const PORT = process.env.PORT || 3000;
 
 //Loading Models:
-const { Galaxies } =require('./src/models/')
+const { Galaxies } = require("./src/models/");
 
 // Create a new Express instance called "app"
-const app = express()
+const app = express();
 
 // ***Loading Twig***
-app.set('views', __dirname + '/src/views')
-
-// **Set Twig as our rendering agent***
-app.set('view engine', 'twig' )
+app.set("view engine", "twig");
+app.set("views", __dirname + "/templates/views/");
 
 // ***Load the JSON body parser***
 app.use(express.json());
 // Load in our RESTful routers
-const routers = require('./routers/index.js')
+const routers = require("./routers/index.js");
 
 // Home page welcome middleware
-app.get('/', async (req, res) => {
-  const id = req.params.id;
+app.get("/", async (req, res) => {
   const galaxy = await Galaxies.findByPk(1);
-  res
-   // .status(200)
-  //  .send('Welcome to Star Tracker Library')
-    .render('home',{ galaxy  }
-    )
-})
+  try {
+    res.render("home/home.twig", { galaxy });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: `${req.method}failed, consult error >>>${error}`,
+    });
+  }
+});
 
 // Register our RESTful routers with our "app"
-app.use(`/planets`,  routers.planet)
-app.use(`/stars`,    routers.star)
-app.use(`/galaxies`, routers.galaxy)
+app.use(`/planets`, routers.planet);
+app.use(`/stars`, routers.star);
+app.use(`/galaxies`, routers.galaxy);
 
 // Set our app to listen on port 3000
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
--- Assignments/Star-Tracker/routers/galaxy.js
+++ Assignments/Star-Tracker/routers/galaxy.js
@@ -1,18 +1,24 @@
 // Load in Express framework
-const express = require(`express`)
+const express = require(`express`);
 
 // Load in our controller/action instances
-const galaxyCtlr = require(`../controllers/galaxy.js`)
+const galaxyCtlr = require(`../controllers/galaxy.js`);
 
 // Create a new Router instance and call it "router"
-const router = new express.Router()
+const router = new express.Router();
 
-// RESTful resource mappings
-router.get(`/`, galaxyCtlr.index)
-router.post(`/`, galaxyCtlr.create)
-router.get(`/:id`, galaxyCtlr.show) 
-router.put(`/:id`, galaxyCtlr.update) 
-router.delete(`/:id`, galaxyCtlr.remove) 
+// RESTful resource mappings (JSON)
+router.get(`/`, galaxyCtlr.index);
+router.post(`/`, galaxyCtlr.create);
+router.get(`/:id`, galaxyCtlr.show);
+router.put(`/:id`, galaxyCtlr.update);
+router.delete(`/:id`, galaxyCtlr.remove);
+
+// HTML5 Routes:
+router.get("/new", galaxyCtlr.form);
+router.get(":id/edit", galaxyCtlr.form);
+router.get("/:id/delete", galaxyCtlr.remove);
+router.post("/:id", galaxyCtlr.update);
 
 // export "router"
-module.exports = router
+module.exports = router;
